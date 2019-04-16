2ebf631cd13a84547c89abc62e76fa2061b430dd
Jean Carlos Moreira Da Silva <jean.moreira@castinformatica.onmicrosoft.com>
2019-04-16 14:41:07 -0300

    add unit tests

---
+++ test/UnitTests/FeedbackControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tot.Api.Controllers;
+using Tot.Infra.Bus;
+using Tot.Query.Models;
+using Tot.Query.Queries.Feedbacks;
+
+namespace UnitTests
+{
+    public class FeedbackControllerTests
+    {
+
+        [Test]
+        public async Task GetAllSucceeds()
+        {
+            // arrange
+            var bus = new Mock<IMediatorHandler>();
+
+            var expectedFeedbacks = new FeedbackListQueryModel(new List<FeedbackViewQueryModel>());
+                       
+            bus.Setup(x => x.ExecuteQuery(It.IsAny<GetFeedbackListQuery>()))
+               .ReturnsAsync(expectedFeedbacks);
+
+            var api = new FeedbackController(bus.Object);
+
+            // act
+
+            var feedbacksRetrieved = await api.List();
+
+            //assert
+
+            feedbacksRetrieved.Count().Should().Be(expectedFeedbacks.Count());
+        }
+
+    }
+}
---
+++ test/UnitTests/FeedbackTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using Tot.Shared.Models;
+using System;
+
+namespace UnitTests
+{
+    public class FeedbackTests
+    {
+        [Test]
+        public void IsUnique() => Feedback.CreateNewFeedback("Teste", default).Id.Should().NotBeEmpty();
+
+        [Test]
+        public void IsCurrentDate() => Feedback.CreateNewFeedback("Teste", default).Creation.Date.Should().Be(DateTime.Now.Date);
+    }
+}
